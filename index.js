42f07d0cc4b9424f48c2911f2cb21114756d64ac
Abdullah <uwannavers901@gmail.com>
2025-10-07 14:08:09 +0700

    UPDATE COOKIE

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 30 * 24 * 60 * 60 * 1000, //30 Hari
     },
   })
